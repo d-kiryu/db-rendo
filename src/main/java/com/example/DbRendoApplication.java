8b82022541bdddeb96532013cd7c43aa744d1323
d.k <none>
2016-11-20 15:17:17 +0900

    db-rendo commit1

---
+++ src/main/java/com/example/DbRendoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRendoApplication.class, args);
+	}
+}
